0cdf5fca713691e3b0aa448f4f636b3486dbb76b
Lee Ford <leeford@users.noreply.github.com>
2018-07-31 15:53:35 +0100

    Fixed SIP replace

--- Get-CSSessions.ps1
+++ Get-CSSessions.ps1
@@ -253,7 +253,7 @@ function ProcessUserSessions($User, $Counter) {
     
     # Strip sip: from SIP Address
     $SipURI = $User.SipAddress.Replace("sip:","")
-    $SipURI = $User.SipAddress.Replace("SIP:","")
+    $SipURI = $SipURI.Replace("SIP:","")
     
     Write-Host "`nChecking $SipURI for sessions..."
 
